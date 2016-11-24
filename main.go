4ef85d0aa8f2025c07641b2472f966b43b9e1406
Tweenagedream <wadepiotrowski@gmail.com>
2016-11-23 19:57:20 -0800

    More fun

--- main.go
+++ main.go
@@ -8,11 +8,53 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 )
+
+var conns map[int32]*websocket.Conn
+var id, gCount int32
+var idLock sync.Mutex
+var countLock sync.Mutex
+
+func getId() int32 {
+	idLock.Lock()
+	defer idLock.Unlock()
+	ret := id
+	id++
+	return ret
+}
+
+func broadcastToConns(data []byte, err error) error {
+	for conn := range conns {
+		err = websocket.Message.Send(conns[conn], data)
+	}
+	return err
+}
+
+func addToCount(addition int32) error {
+	countLock.Lock()
+	defer countLock.Unlock()
+	gCount += addition
+	fmt.Fprintf(os.Stderr, "New gCount value: %v\n", gCount)
+	creply := &count.CountReply{
+		Count: gCount,
+	}
+	err := broadcastToConns(proto.Marshal(creply))
+	return err
+}
+
+func deleteAndLog(connId int32) {
+	fmt.Fprintf(os.Stderr, "Deleting connection: %v\n", connId)
+	delete(conns, connId)
+}
 
 func WSServer(ws *websocket.Conn) {
 	var buffer []byte
 	var counter int32
+	connId := getId()
+	conns[connId] = ws
+	defer deleteAndLog(connId)
+	fmt.Fprintf(os.Stderr, "New connection: %v\n", connId)
 	websocket.Message.Receive(ws, &buffer)
 	helloReq := &count.HelloRequest{}
 	proto.Unmarshal(buffer, helloReq)
@@ -22,10 +64,30 @@ func WSServer(ws *websocket.Conn) {
 		Ack: *proto.Bool(true),
 	})
 	websocket.Message.Send(ws, buffer)
+	countRequest := &count.CountRequest{}
+	ok := true
+	var err error
+	for ok {
+		websocket.Message.Receive(ws, &buffer)
+		err = proto.Unmarshal(buffer, countRequest)
+		fmt.Fprintf(os.Stderr, "Value of buffer: <%v>\n", buffer)
+		if err != nil {
+			ok = false
+			break
+		}
+		if countRequest.GetCount() > 0 {
+			err = addToCount(countRequest.GetCount())
+			if err != nil {
+				ok = false
+				break
+			}
+		}
+	}
 
 }
 
 func main() {
+	conns = make(map[int32]*websocket.Conn)
 	http.Handle("/ws/", websocket.Handler(WSServer))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
@@ -33,4 +95,5 @@ func main() {
 	http.Handle("/static/", http.FileServer(http.Dir("protos")))
 
 	http.ListenAndServe(":8000", nil)
+	fmt.Fprintf(os.Stderr, "Listening on port 8000...")
 }
